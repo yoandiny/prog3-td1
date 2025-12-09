46d550f35bdf877c19ae3ba1e25b40abf4ebba82
Yoan <yoandiny14@gmail.com>
2025-12-09 17:26:48 +0300

    Question four answered

---
+++ src/main/java/Category.java
@@ -0,0 +1,4 @@
+public class Category {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,17 @@
+import java.time.Instant;
+
+public class Product {
+    private int id;
+    private String name;
+    private Instant creationDatetime;
+    private Category Category;
+
+    public Product(int id, String name, Instant creationDatetime, Category category) {
+        this.id = id;
+        this.name = name;
+        this.creationDatetime = creationDatetime;
+        Category = category;
+    }
+
+
+}
