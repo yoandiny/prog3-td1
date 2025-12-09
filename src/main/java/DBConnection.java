a9966e9d67fe824145817c8a84473385189b1b9f
Yoan <yoandiny14@gmail.com>
2025-12-09 18:14:31 +0300

    Question five answered

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,2 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private final String USER = "product_manager_user";
+    private final String PASSWORD = "123456";
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void main(String[] args) {
+        Connection conn = getConnection();
+        if (conn != null) {
+            System.out.println("Connexion réussie !");
+        }
+    }
 }
